f71c8cf1c83445904c319ac3e0ca57f99824a7db
ice2323 <32441294+ice2323@users.noreply.github.com>
2020-05-15 23:15:17 -0300

    Update cipher.c
    fixed cipher worked on pthread implementation.

--- cipher.c
+++ cipher.c
@@ -1,78 +1,108 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <string.h>
-#include <stdbool.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
 
-int cores;
-int fd[5][2];
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+#define SIZE 15
+int to_child_fd[2];
+int to_parent_fd[2];
+    char message[SIZE];
 
 
 void * doWork(void * arg){
 
-	int n = 0;
-	char letter; 
+	char message[SIZE];
+	int result;
+	int shiftLength = *((int *) arg);
+	int ch;
+	char sentences[SIZE];
 
-	pthread_mutex_lock(&lock);
+	read(to_parent_fd[0], message, SIZE);
 
-	while(1){
+	int msgSize = strlen(message);
 
-		if(read(fd[n][0], &letter, sizeof(char))){
+	for(int i = 0; i < msgSize; i++){
 
-			if(letter == '\0'){
-				break;
+		ch = message[i];
+
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch + shiftLength;
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1;
+			}else if(ch < 'a'){
+				ch = ch + 26;
 			}
-
-			printf("%c\n", letter);
-	
-		}
+		}else if(ch >= 'A' && ch <= 'Z'){
+				ch = ch + shiftLength;
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}else if(ch < 'A'){
+				ch = ch + 26;
+			}
+		}		
+		sentences[i] = ch;
 	}
-
-	
-
-
-	pthread_mutex_unlock(&lock);
+	for(int i =  0; i < msgSize; i++){
+		printf("%c", sentences[i]);
+	}
+	printf("\n");
+	write(to_parent_fd[1], sentences,  100);
 }
 
 int main(int argc, char * argv[]){
+    
+    int shiftList[] = {};
+    int value;
+    int counter = 0;
+    int cores = 0;
 
-	int shiftList[1000] = {};
-	int i = 0;
-	char letterString[1000] = {};
+    for(int i = 1; i < argc; i++){
 
-	printf("%s\n", "Enter your string: ");
+    	shiftList[counter] = atoi(argv[i]);
+    	counter++;
+    }
+    int *currentShift = malloc(sizeof(*currentShift));
+    *currentShift = shiftList[0];
 
-	fgets(letterString, 1000, stdin);
+	pipe(to_child_fd);
+    pipe(to_parent_fd);
+    if (fork() == 0) {
+    int length;
 
-	letterString[strlen(letterString) -1] = '\0';
+    close(to_child_fd[1]);  /* child closes write side of child  pipe */
+    close(to_parent_fd[0]); /* child closes read  side of parent pipe */
+    length = read(to_child_fd[0], message, SIZE);
+    
+    write(to_parent_fd[1], message, strlen(message) + 1);
+    close(to_parent_fd[1]);
+    } else {
 
-	cores = argc - 1;
+    char phrase[SIZE];
+    char message[SIZE];
+    printf("please type a sentence : ");
+    scanf("%s", phrase);
+    close(to_parent_fd[1]); /* parent closes write side of parent pipe */
+    close(to_child_fd[0]);  /* parent closes read  side of child  pipe */
+    write(to_child_fd[1], phrase, strlen(phrase) + 1);
+    //close(to_child_fd[1]);
+    //read(to_parent_fd[0], message, SIZE);
 
-	pthread_t tid[1];
-
-	for(int i = 0; i < argc - 1; i++){
-
-		pthread_create(&tid[i], NULL, &doWork, NULL);
-	}
-
-	for(int i = 0; i < cores + 1; i++){
-
-		pipe(fd[i]);
-	}
-	for(int i = 0; i < strlen(letterString); i++){
-
-		write(fd[i][1], &letterString[i], sizeof(int));
-	}
-	close(fd[0][1]);
-
-	pthread_join(tid[0], NULL);
-	/**
-	Where fds[i][0] represents read end and fds[i][1] represents write end.
-
-	*/
+    pthread_t tid[1];
 
 
+    pthread_create(&tid[0], NULL, &doWork, currentShift);
+    //sleep(10);
+    pthread_join(tid[0], NULL);
+
+    //pthread_create(&tid[1], NULL, &doWork, NULL);
+
+
+
+    //pthread_join(tid[1], NULL);
+
+    }
+    return 0;
 }
